445a92aa0dd332f23c4df9c08eac126d80aea93a
5P1AM <prokopovaste@gmail.com>
2023-11-22 20:23:40 +0000

    poslat

---
+++ program.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int random_(int min, int max) {
+
+     return rand() % (max - min + 1) + min;
+}
+
+int main() {
+
+    // Number of records in the dataset
+    const int DATASET_SIZE = 100;
+
+    // Seed random number generator with current time
+    srand(time(NULL));
+
+    printf("%s\n", // Dataset header (must match the number of columns)
+        "fce1,fce2,třída"
+    );
+
+    for (int i = 0; i < DATASET_SIZE; i++) {
+
+        printf(
+            "%d,%d,%s\n",
+            random_(0, 10),               //fce1      
+            random_(12, 25),                  //fce2
+            random_(0, 1) ? "\"y\"" : "\"n\""  //třída
+        );
+    }
+
+    return 0;
+}
